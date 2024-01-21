8677922025cf71ae029ed24ebfd709aee369ea9d
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-21 16:10:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,14 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+#问题：git clone -b master https://github.com/sirpdboy/luci-theme-kucat.git和
+#      git clone --depth=1 -b master https://github.com/sirpdboy/luci-theme-kucat  的效果一样吗？
+#回答：两个命令都是用于克隆Git仓库的，有一些细微的差别：
+#git clone -b master https://github.com/sirpdboy/luci-theme-kucat.git：这个命令克隆整个仓库（luci-theme-kucat.git）并检出master分支。
+#git clone --depth=1 -b master https://github.com/sirpdboy/luci-theme-kucat：这个命令同样克隆仓库并检出master分支，但使用了--depth=1选项。这个选项创建一个浅克隆，只包含最新的提交，没有超过指定深度的提交历史。
+#这两个命令的实际效果是，第二个命令（--depth=1）创建了一个更小的克隆，只包含最新的提交，而没有完整的提交历史。这对于只关注仓库的最新状态并且想要节省带宽和磁盘空间的情况可能会很有用。
+#总的来说，就代码而言，这两个命令的结果可能是一样的，但第二个命令（--depth=1）创建了一个更小的克隆，没有完整的历史记录。
+      
 
 # Uncomment a feed source
 # 将原luci注释掉
@@ -61,11 +69,14 @@ rm -rf openwrt-packages
 rm -rf ./feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-bootstrap
+rm -rf feeds/luci/themes/luci-theme-Bootstrap
+rm -rf feeds/luci/themes/luci-theme-BootstrapDark
+rm -rf feeds/luci/themes/luci-theme-BootstrapLight
 
 cd package
 git clone https://github.com/f8q8/luci-app-autoreboot.git
 git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git
+git clone -b main https://github.com/sirpdboy/luci-app-advancedplus.git
 cd ..
 
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
